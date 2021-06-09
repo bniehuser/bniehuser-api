0e421da8aedcaa56d93f69eead17484276bd2358
Barry Niehuser <bniehuser@gmail.com>
2021-06-08 23:26:33 -0700

    swap for FastAPI

--- tests/test_blueprints/test_root.py
+++
@@ -1,7 +0,0 @@
-from http import HTTPStatus
-
-
-def test_health(client):
-    response = client.get('/health/')
-    assert response.status_code == HTTPStatus.OK, 'Health check failed'
-    assert response.json == {'message': 'Healthy'}, 'Improper response'
